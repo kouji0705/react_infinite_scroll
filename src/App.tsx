8a1cf5d39fcb7bd9217c141af940370af58db331
kouji_kawamura <45913424+kouji0705@users.noreply.github.com>
2023-10-28 15:31:36 +0900

    add: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react';
+import { useInfinityScroll } from './hooks/useInfinityScroll'
 
 function App() {
+  const containerRef = useRef(null);
+
+  const fetchData = async (page: number) => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_page=${page}`);
+    const data = await response.json();
+    return data;
+  };
+
+  const data: Post[] = useInfinityScroll(containerRef, fetchData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{height: '2000px'}}>Scroll down</div>
+      <div ref={containerRef}>
+        {data.map((item: Post) => (
+          <div key={item.id}>
+            <p>
+              {item.id}：{item.title}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
 export default App;
+
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
---
+++ src/hooks/useInfinityScroll.tsx
@@ -0,0 +1,45 @@
+import {RefObject, useCallback, useEffect, useState} from 'react';
+
+export const useInfinityScroll = <T,>(ref: RefObject<HTMLElement | null>, fetch: (page: number) => Promise<T[]>) => {
+  const [data, setData] = useState<T[]>([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false); // 読み込み中のフラグ
+  const [hasMoreData, setHasMoreData] = useState(true); // 追加データがあるかどうかを追跡
+
+  const scrollObserver = useCallback(
+    () =>
+      new IntersectionObserver((entries) => {
+        console.log('entries', entries);
+        entries.forEach((entry) => {
+          if (entry.isIntersecting && !isLoading && hasMoreData) {
+            setIsLoading(true); // 読み込み中フラグを設定
+            fetch(page).then((_data) => {
+              console.log('fetch call page', page);
+              if (_data.length > 0) {
+                setPage(page + 1);
+                setData((oldValue) => [...oldValue, ..._data]);
+              } else {
+                // 追加データがない場合
+                setHasMoreData(false);
+              }
+              setIsLoading(false);
+            });
+          }
+        });
+      }),
+    [page, fetch, isLoading, hasMoreData]
+  );
+
+  useEffect(() => {
+    const target = ref.current;
+    if (target) {
+      const observer = scrollObserver();
+      observer.observe(target);
+      return () => {
+        observer.unobserve(target);
+      };
+    }
+  }, [scrollObserver, ref]);
+
+  return data;
+};
